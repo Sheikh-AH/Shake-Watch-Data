8733d19931a841ef6953798ec0ab204f2c7019ae
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-01-01 16:35:24 +0000

    Add columns for activity summary data and streams in schema.sql.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,19 @@
 CREATE TABLE IF NOT EXISTS activities (
-    activity_id PRIMARY KEY
+    activity_id INTEGER PRIMARY KEY
+    activity_name VARCHAR(255), --name in api data
+    calories INTEGER,
+    distance FLOAT,
+    moving_time INTEGER,
+    elapsed_time INTEGER,
+    total_elevation_gain INTEGER,
+    activity_type VARCHAR(20), --sport_type in api data
+    start_at TIMESTAMP --start_date_local in api data
+    location_city VARCHAR(30)
+)
+
+CREATE TABLE IF NOT EXISTS stream_sets (
+    stream_sets_id INTEGER PRIMARY KEY,
+    activity_id INTEGER,
+    streams BLOB NOT NULL,
+    FOREIGN KEY (activity_id) REFERENCES activities(activity_id) ON DELETE CASCADE
 )
