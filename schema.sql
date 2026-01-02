f7aa6c47b29d908749e7c113d3a3eec972341bab
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-01-02 19:36:59 +0000

    Add function for connection to sqlite database.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS activities (
-    activity_id INTEGER PRIMARY KEY
+    activity_id INTEGER PRIMARY KEY,
     activity_name VARCHAR(255), --name in api data
     calories INTEGER,
     distance FLOAT,
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS activities (
     elapsed_time INTEGER,
     total_elevation_gain INTEGER,
     activity_type VARCHAR(20), --sport_type in api data
-    start_at TIMESTAMP --start_date_local in api data
+    start_at TIMESTAMP, --start_date_local in api data
     location_city VARCHAR(30)
-)
+);
 
 CREATE TABLE IF NOT EXISTS stream_sets (
     stream_sets_id INTEGER PRIMARY KEY,
