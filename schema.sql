eac4fac2e00fa691b17c4d6d1dbc6b2f31cb8b78
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-01-10 20:01:00 +0000

    Add all_streams and test_data.

--- schema.sql
+++ schema.sql
@@ -1,15 +1,14 @@
-CREATE TABLE IF NOT EXISTS activity_types (
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS activity_types;
+DROP TABLE IF EXISTS stream_sets;
+
+
+CREATE TABLE activity_types (
     activity_type_id INTEGER PRIMARY KEY,
     activity_type_name VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS cities (
-    city_id INTEGER PRIMARY KEY,
-    city_name VARCHAR(30) NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS activities (
+CREATE TABLE activities (
     activity_id INTEGER PRIMARY KEY,
     activity_name VARCHAR(255), --name in api data
     calories INTEGER,
@@ -18,13 +17,12 @@ CREATE TABLE IF NOT EXISTS activities (
     elapsed_time INTEGER,
     total_elevation_gain INTEGER,
     activity_type_id VARCHAR(20) NOT NULL, --sport_type in api data
-    start_at TIMESTAMP, --start_date_local in api data
-    city_id VARCHAR(30),
-    FOREIGN KEY (activity_type_id) REFERENCES activity_types(activity_type_id) ON DELETE NO ACTION,
-    FOREIGN KEY (city_id) REFERENCES cities(city_id) ON DELETE NO ACTION
+    start_time TIMESTAMP, --start_date_local in api data
+    start_loc VARCHAR(30) DEFAULT 1, --start_latlng in api data
+    FOREIGN KEY (activity_type_id) REFERENCES activity_types(activity_type_id) ON DELETE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS stream_sets (
+CREATE TABLE stream_sets (
     stream_sets_id INTEGER PRIMARY KEY,
     activity_id INTEGER,
     streams BLOB NOT NULL,
@@ -32,11 +30,8 @@ CREATE TABLE IF NOT EXISTS stream_sets (
 );
 
 INSERT INTO activity_types (activity_type_name)
-VALUES (
-    'Running'
-);
-
-INSERT INTO cities (city_name)
-VALUES (
-    'London'
-);
+VALUES 
+    ('Run'),
+    ('Walk'),
+    ('WeightTraining')
+;
