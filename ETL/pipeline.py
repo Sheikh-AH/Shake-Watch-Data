c4c50b7e0b734380b2f86827418ac83eabd8cf8a
Sheikh Abdul Halim <trainee.sheikh.abdul.halim@sigmalabs.co.uk>
2026-01-14 11:20:30 +0000

    Remove redundant imports and arguments.

--- ETL/pipeline.py
+++ ETL/pipeline.py
@@ -9,9 +9,9 @@ from load import upload_activities, upload_streams
 def etl_pipeline(conn, config: _Environ):
     """Run the ETL pipeline."""
     check_access_token(config)
-    activities_detailed, streams = extract_data(conn, ENV)
+    activities_detailed, streams = extract_data(conn, config)
     clean_activities_data, clean_streams_data = clean_data(
-        conn, ENV, (activities_detailed, streams))
+        conn, (activities_detailed, streams))
     upload_activities(conn, clean_activities_data)
     upload_streams(conn, clean_streams_data)
 
@@ -19,6 +19,6 @@ def etl_pipeline(conn, config: _Environ):
 if __name__ == '__main__':
 
     load_dotenv()
-    conn = get_connection()
-    etl_pipeline(conn, ENV)
-    conn.close()
+    connection = get_connection(ENV)
+    etl_pipeline(connection, ENV)
+    connection.close()
--- ETL/transform.py
+++ ETL/transform.py
@@ -1,15 +1,6 @@
 """Process and transform data extracted from the API."""
 
-from os import environ as ENV, _Environ
-from dotenv import load_dotenv
-from datetime import datetime
-
-from sqlite3 import Connection
-
-from extract import get_connection, extract_data
-
-
-def get_type_mapping(conn: Connection) -> dict:
+def get_type_mapping(conn) -> dict:
     """Retrieve activity type mapping from the database."""
     cursor = conn.cursor()
     cursor.execute(
@@ -26,7 +17,7 @@ def convert_types_to_ids(mapping: dict, activities: list[dict]) -> list[dict]:
     return activities
 
 
-def clean_activities(conn: Connection, activities_detailed: list[dict]) -> list[dict]:
+def clean_activities(conn, activities_detailed: list[dict]) -> list[dict]:
     """Process activities data."""
     type_mapping = get_type_mapping(conn)
     activities = convert_types_to_ids(type_mapping, activities_detailed)
@@ -60,9 +51,9 @@ def filer_all_streams(streams: list[dict]) -> list[dict]:
     return [(stream[0], filter_streams(stream[1])) for stream in streams]
 
 
-def clean_data(conn, ENV: _Environ, extract_data: tuple) -> tuple:
+def clean_data(conn, data: tuple) -> tuple:
     """Main function to clean and transform data."""
-    activities_detailed, streams = extract_data[0], extract_data[1]
+    activities_detailed, streams = data[0], data[1]
     clean_activities_data = clean_activities(conn, activities_detailed)
     clean_streams_data = filer_all_streams(streams)
     return clean_activities_data, clean_streams_data
