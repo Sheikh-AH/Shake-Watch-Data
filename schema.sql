15bd2c4945ffbd29227e6db57f9c8c4f3b70b6d2
unknown <life.shake@outlook.com>
2026-03-26 15:48:33 +0000

    Update schema.sql to remove activity types and add effort/pace.

--- schema.sql
+++ schema.sql
@@ -7,10 +7,6 @@ DROP DATABASE IF EXISTS watch_data;
 CREATE DATABASE watch_data;
 \c watch_data;
 
-CREATE TABLE activity_types (
-    activity_type_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    activity_type_name VARCHAR(20) NOT NULL
-);
 
 CREATE TABLE activities (
     activity_id BIGINT PRIMARY KEY,
@@ -20,10 +16,10 @@ CREATE TABLE activities (
     moving_time INT,
     elapsed_time INT,
     total_elevation_gain INT,
-    activity_type_id INT NOT NULL, --sport_type in api data
     start_datetime TIMESTAMP, --start_date_local in api data
     start_loc NUMERIC[], --start_latlng in api data
-    FOREIGN KEY (activity_type_id) REFERENCES activity_types(activity_type_id) ON DELETE NO ACTION
+    effort INT,
+    pace NUMERIC
 );
 
 CREATE TABLE stream_sets (
@@ -42,11 +38,3 @@ CREATE TABLE stream_sets (
     grade_smooth NUMERIC[],
     FOREIGN KEY (activity_id) REFERENCES activities(activity_id) ON DELETE CASCADE
 );
-
-INSERT INTO activity_types (activity_type_name)
-VALUES 
-    ('Run'),
-    ('Walk'),
-    ('WeightTraining'),
-    ('Workout')
-;
