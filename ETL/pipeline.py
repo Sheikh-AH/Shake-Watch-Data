159b95cda66cd9d8bc123d4de50335d15502786f
unknown <life.shake@outlook.com>
2026-04-16 14:35:51 +0100

    Change var names to be clearer and base directory path.

--- ETL/pipeline.py
+++ ETL/pipeline.py
@@ -1,12 +1,17 @@
 """Gather all ETL pipeline components."""
 from os import environ as ENV, _Environ
 from dotenv import load_dotenv
+from pathlib import Path
+import sys
 
 import streamlit as st
 
-from .extract import extract_data, get_connection
-from .transform import clean_data
-from .load import upload_activities, upload_streams
+BASE_DIR = Path(__file__).resolve().parent
+sys.path.append(str(BASE_DIR))
+
+from extract import extract_data, get_connection
+from transform import transform_data
+from load import upload_activities, upload_streams
 
 
 def etl_pipeline(config: _Environ):
@@ -16,9 +21,9 @@ def etl_pipeline(config: _Environ):
     activities_detailed, streams = extract_data(conn, config, update_check=True)
 
     if activities_detailed:
-        clean_activities, clean_streams = clean_data((activities_detailed, streams))
-        upload_activities(conn, clean_activities)
-        upload_streams(conn, clean_streams)
+        transformed_acts, transformed_strms = transform_data(config, (activities_detailed, streams))
+        upload_activities(conn, transformed_acts)
+        upload_streams(conn, transformed_strms)
         st.info(f'{len(activities_detailed)} new activities added.')
     else:
         print('No new activities.')
